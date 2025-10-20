de442efc39d1574fa0f6dd0f51d11c1e441e1098
jovita ayu <jovitaayr@gmail.com>
2025-10-20 13:49:14 +0700

    add .gitignore

--- server/index.js
+++ server/index.js
@@ -1,55 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
-const connectDB = require("./api/config/connectDB");
-const cors = require("cors");
-const port = process.env.PORT || 5000;
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
+const PORT = 5000;
 
-//CONFIG ENV
-dotenv.config();
-
-//CONNECT TO DB
-connectDB(); 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors());
-
-//IMPORT ROUTES
-// const userRouter = require("./api/routes/userRoutes");
-
 app.get("/", (req, res) => {
-  res.send("API Capstone E09");
+  res.send('Welcome to Waste Collection Management API');
 });
 
-//ROUTES
-// app.use("/user", userRouter);
-
-
-// ? Error handler\
-// ? will be called automatically when the url doesn't exist or it's wrong
-app.use((req, res, next) => {
-  const error = new Error("Not found!");
-  error.status = 404;
-  next(error);
-});
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
-});
-// ? End error handling
-
-//SERVER RUN
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(
-    `Server backend Capstone E09 running on port http://localhost:${port}`
+    `Server backend Capstone E09 running on port http://localhost:${PORT}`
   );
 });
