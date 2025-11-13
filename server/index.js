a3c27958b70cd17ea8062ad4dcd33c8ebe44022b
Zidane <114844442+MZidane28@users.noreply.github.com>
2025-11-13 22:11:22 +0700

    Add optimize router and clean up MQTT logging

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import mqtt from "mqtt";
 
 import connectDB from "./configs/database.js";
 import binRouter from "./routes/binRoutes.js";
-import simulationRouter from "./routes/simulationRoutes.js";
 import solutionRouter from "./routes/solutionRoutes.js";
+import optimizeRouter from "./routes/optimizeRoutes.js";
 
+import mqtt from "mqtt";
 dotenv.config();
 
 connectDB();
@@ -20,12 +20,13 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/v1/bins', binRouter);
-app.use('/api/v1/solutions', solutionRouter);
-app.use('/api/v1/simulation', simulationRouter);
+app.use('/api/v1/bins',binRouter);
+app.use('/api/v1/solutions',solutionRouter);
+app.use('/api/v1/optimize', optimizeRouter);
 
 app.get("/", (req, res) => {
   res.send('Welcome to Waste Collection Management API');
+  res.send("API Capstone E09");
 });
 
 //ROUTES
@@ -43,7 +44,7 @@ client.on("connect", () => {
   console.log("✅ Connected to MQTT broker");
   client.subscribe(topicPot, (err) => {
     if (!err) {
-      console.log(`📡 Subscribed to topic: ${topicPot}`);
+      console.log(📡 Subscribed to topic: ${topicPot});
     } else {
       console.error("Subscribe error:", err);
     }
@@ -54,7 +55,7 @@ client.on("connect", () => {
 client.on("message", (topic, message) => {
   if (topic === topicPot) {
     const data = message.toString();
-    console.log(`📥 Data received [${topic}]: ${data}`);
+    console.log(📥 Data received [${topic}]: ${data});
 
     // Misal parsing data "ADC,Voltage"
     const [adc, voltage] = data.split(",");
@@ -63,7 +64,8 @@ client.on("message", (topic, message) => {
 });
 // ------------------END MQTT--------------
 
-// ? Error handler
+// ? Error handler\
+// ? will be called automatically when the url doesn't exist or it's wrong
 app.use((req, res, next) => {
   const error = new Error("Not found!");
   error.status = 404;
@@ -80,6 +82,6 @@ app.use((error, req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(
-    `Server backend Capstone E09 running on port http://localhost:${PORT}`
+    Server backend Capstone E09 running on port http://localhost:${PORT}
   );
 });
